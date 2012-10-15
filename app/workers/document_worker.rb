45d117f77ee6d7dffa389662ff66fe42cea1cc10
Alex Sayers <alex.sayers@gmail.com>
2012-10-15 23:24:36 +0100

    Change rendering to use rubber rather than rtex

--- app/workers/document_worker.rb
+++ app/workers/document_worker.rb
@@ -1,31 +1,48 @@
 class DocumentWorker
   include Sidekiq::Worker
+  sidekiq_options retry: false
 
   def perform(id)
-    # TODO: create a temporary directory
+    # Fetch records; create directory
     doc = Document.includes(:bibliography).find(id)
     bib = doc.bibliography ? doc.bibliography : Bibliography.find(1)
-    # TODO: change these to source.tex and biblio.bib
-    source = Tempfile.open(["source",".tex"], Rails.root.join("tmp/render"))
-    biblio = Tempfile.open(["biblio",".bib"], Rails.root.join("tmp/render"))
+    dir = Dir.mktmpdir("scientia-")
+    
+    # Write source files
+    source = File.open(dir+"/source.tex", 'w')
+    biblio = File.open(dir+"/biblio.bib", 'w')
+    source.puts doc.latex
+    biblio.puts bib.body
+    source.close
+    biblio.close
 
+    # Render
     begin
-      # TODO: change "<< biblio >>" to "biblio.bib" in the template
-      source.puts doc.latex
-      biblio.puts bib.body
+      puts "=> Beginning render...    (#{doc.name})"
+      system("rubber --inplace --pdf #{source.path}")
     rescue
       # TODO: try to reclaim the errors
+      puts "=> Something went wrong!  (#{doc.name})"
       doc.warnings = "Latex rendering error!"
     else
+      puts "=> Render successful!     (#{doc.name})"
+      output = File.open(dir+"/source.pdf", 'r')
+      doc.pdf = output.read
+      output.close
       doc.warnings = nil
     ensure
-      # TODO: probably gonna have to destroy the directory here
-      source.close
-      biblio.close
-      # TODO: check it saves correctly?
+      # Tidy up; save
+      FileUtils.remove_entry_secure dir
       doc.save
     end
-
+      
+#     The same, but with rubber-pipe?
+#     IO.popen("rubber-pipe") do |io|
+#       io.puts doc.latex
+#       io.gets ...
+#     end
+#     if $? == 0 ...
+   
 #    rtex = RTeX::Document.new(doc.latex)
 #    begin
 #      doc.pdf = rtex.to_pdf
--- app/workers/template_worker.rb
+++ app/workers/template_worker.rb
@@ -1,20 +1,43 @@
 class TemplateWorker
   include Sidekiq::Worker
+  sidekiq_options retry: false
 
   def perform(id)
+    # Fetch records; create directory
     template = Template.find(id)
     latex, html = Document.find(1).compile(template.body)
-    rtex = RTeX::Document.new(latex)
+    bib = Bibliography.find(1).body
+    dir = Dir.mktmpdir("scientia-")
+    
+    # Write source files
+    source = File.open(dir+"/source.tex", 'w')
+    biblio = File.open(dir+"/biblio.bib", 'w')
+    source.puts latex
+    biblio.puts bib
+    source.close
+    biblio.close
+
+    # Render
     begin
-      template.pdf = rtex.to_pdf
+      puts "=> Beginning render...        (#{template.name})"
+      system("rubber --inplace --pdf #{source.path}")
     rescue
+      # TODO: try to reclaim the errors
+      puts "=> Something went wrong!      (#{template.name})"
       template.warnings = "Latex rendering error!"
     else
+      puts "=> Render successful!         (#{template.name})"
+      output = File.open(dir+"/source.pdf", 'r')
+      template.pdf = output.read
+      output.close
       template.warnings = nil
     ensure
+      # Tidy up; save
+      FileUtils.remove_entry_secure dir
       template.save
     end
 
+    # Recompile related documents
     if template.warnings = nil
       template.documents.each { |d| d.regenerate }
     end
