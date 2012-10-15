bb8f1d7a7b7ecf4635bb522d4289dbc360090e70
Alex Sayers <alex.sayers@gmail.com>
2012-10-15 13:25:28 +0100

    Add gravatar; add "more" modal; add flash timeout
    Also, many styling changes.

--- app/assets/javascripts/alert.js.coffee
+++ app/assets/javascripts/alert.js.coffee
@@ -1,6 +1,10 @@
 jQuery ->
   $(".alert").hide().fadeIn()
   
+  setTimeout ( ->
+    $(".alert").fadeOut()
+  ), 3000
+  
   $(".alert").click( ->
     $(this).fadeOut()
   )
--- app/workers/document_worker.rb
+++ app/workers/document_worker.rb
@@ -2,16 +2,39 @@ class DocumentWorker
   include Sidekiq::Worker
 
   def perform(id)
-    doc = Document.find(id)
-    rtex = RTeX::Document.new(doc.latex)
+    # TODO: create a temporary directory
+    doc = Document.includes(:bibliography).find(id)
+    bib = doc.bibliography ? doc.bibliography : Bibliography.find(1)
+    # TODO: change these to source.tex and biblio.bib
+    source = Tempfile.open(["source",".tex"], Rails.root.join("tmp/render"))
+    biblio = Tempfile.open(["biblio",".bib"], Rails.root.join("tmp/render"))
+
     begin
-      doc.pdf = rtex.to_pdf
+      # TODO: change "<< biblio >>" to "biblio.bib" in the template
+      source.puts doc.latex
+      biblio.puts bib.body
     rescue
-      doc.warnings = "Latex rendering error. Check your template!"
+      # TODO: try to reclaim the errors
+      doc.warnings = "Latex rendering error!"
     else
       doc.warnings = nil
     ensure
+      # TODO: probably gonna have to destroy the directory here
+      source.close
+      biblio.close
+      # TODO: check it saves correctly?
       doc.save
     end
+
+#    rtex = RTeX::Document.new(doc.latex)
+#    begin
+#      doc.pdf = rtex.to_pdf
+#    rescue
+#      doc.warnings = "Latex rendering error. Check your template!"
+#    else
+#      doc.warnings = nil
+#    ensure
+#      doc.save
+#    end
   end
 end
