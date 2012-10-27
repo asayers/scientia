e70b6c5c4e8c580fe9a075a4313987f9756ba916
Alex Sayers <alex.sayers@gmail.com>
2012-10-27 21:21:44 +0100

    Fix deploy.rb for arch

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ namespace :deploy do
 
   task :setup_config, roles: :app do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/rc.d/unicorn_#{application}"
   end
   after "deploy:setup", "deploy:setup_config"
 
