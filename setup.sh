efe50dfb353e94a5800a0ab7b89a9ff99c27ebc1
Alex Sayers <alex.sayers@gmail.com>
2012-10-30 16:08:07 +0000

    Make setup.sh verbose

--- setup.sh
+++ setup.sh
@@ -1,8 +1,16 @@
 #! /bin/bash
 
+echo "Linking nginx config..."
 sudo ln -s $(pwd)/config/nginx.conf /etc/nginx/sites-enabled/scientia
+echo "Installing bundler..."
 gem install bundler
+echo "Installing dependencied"
 bundle install --binstubs
+echo "Creating database..."
 rake db:create
+echo "Migrating database..."
 rake db:migrate
+echo "Seeding database..."
 rake db:seed
+echo "Precompiling assets..."
+rake assets:precompile
