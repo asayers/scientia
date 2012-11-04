53ac3ce56efeca6036a88b65ed6c3ab80e136913
Alex Sayers <alex.sayers@gmail.com>
2012-11-04 03:43:43 +0000

    Add systemd .service files for unicorn and sidekiq

--- config/setup.sh
+++ config/setup.sh
@@ -1,7 +1,14 @@
 #! /bin/bash
 
-echo "Linking nginx config..."
-sudo ln -s $(pwd)/config/nginx.conf /etc/nginx/sites-enabled/scientia
+APP_NAME="scientia"
+
+echo "Linking config files..."
+sudo ln -s $(pwd)/config/nginx.conf /etc/nginx/sites-enabled/$APP_NAME
+sudo cp $(pwd)/config/unicorn.service /usr/lib/systemd/system/unicorn_$APP_NAME.service
+sudo cp $(pwd)/config/sidekiq.service /usr/lib/systemd/system/sidekiq_$APP_NAME.service
+echo "Enabling services"
+sudo systemctl enable unicorn_scientia
+sudo systemctl enable sidekiq_scientia
 echo "Installing bundler..."
 gem install bundler
 echo "Installing dependencied"
