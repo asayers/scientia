9b1700d24aad6808788b81455df5f49b35048bf3
Alex Sayers <alex.sayers@gmail.com>
2012-11-06 19:15:22 +0000

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,13 @@ echo "> Synching repo"
 git pull origin master
 echo "Recompile assets?"
 read ASSETS
-if [ $ASSETS == "y" || $ASSETS == "yes" ]
+if [ $ASSETS == "y" || $ASSETS == "yes" ]; then
   echo "> Precompiling assets..."
   bundle exec rake assets:precompile
 fi
 echo "Migrate database?"
 read MIGRATE
-if [ $MIGRATE == "y" || $MIGRATE == "yes" ]
+if [ $MIGRATE == "y" || $MIGRATE == "yes" ]; then
   echo "> Migrating database..."
   bundle exec rake db:migrate
 fi
