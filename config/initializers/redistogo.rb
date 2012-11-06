e5dc1b3dc4dd7d0e87fdf806cbcf668622964d3c
Alex Sayers <alex.sayers@gmail.com>
2012-11-06 15:55:50 +0000

    Styling for new editor

--- config/initializers/redistogo.rb
+++
@@ -1,4 +0,0 @@
-# Redis database for Sidekiq in the development environment
-unless ENV["RAILS_ENV"] == "production"
-  ENV["REDISTOGO_URL"] = 'redis://asayers:7987993ee766f9fc3ff397f2e450dda5@clingfish.redistogo.com:9641/'
-end
